09ce159ed47a9ad3ed19894593abb46905cd5e12
Soni <soni801_@outlook.com>
2022-03-31 12:52:21 +0200

    feat: handle connection termination

--- src/main/kotlin/net/recalstudios/social/plugins/Sockets.kt
+++ src/main/kotlin/net/recalstudios/social/plugins/Sockets.kt
@@ -20,67 +20,49 @@ fun Application.configureSockets() {
 
     routing {
         val connections = Collections.synchronizedSet<Connection>(LinkedHashSet())
-        webSocket("/e") {
+        webSocket("/") {
             // New connection established
             val connectionId = this.hashCode() // Save unique connection ID
-            var thisConnection: Connection // Declare empty connection
+            var thisConnection: Connection? = null // Declare empty connection
             println("${Date()} [Connection-$connectionId] INFO  Connection attempt, asking for credentials")
 
-
-            // Ask the client for credentials
-            send(Gson().toJson(mapOf("type" to "status", "status" to "auth"))) // Send response
-            var token: String // Declare token object
-
-            // Listen for incoming data
-            for (frame in incoming) {
-                frame as? Frame.Text ?: continue // Assure type of data
-                val received = frame.readText() // Store incoming data
-                val parsed: Map<*, *> // Declare parsed data
-
-                // Try to parse data
-                try {
-                    parsed = Gson().fromJson(received, Map::class.java)
-                } catch (e: Exception) {
-                    println("${Date()} [Connection-$connectionId] INFO  Received bad data, ignoring")
-                    continue
-                }
-
-                // Process data
-                when (parsed["type"])
-                {
-                    "auth" -> {
-                        thisConnection = Connection(this, 0)
-                        connections += thisConnection
-                        println("${Date()} [Connection-$connectionId] INFO  User authenticated, connection accepted (${connections.size} total)")
-                    }
-                }
-            }
-
-
-            /*
+            // Process connection
             try {
-                send(Gson().toJson(mapOf<Any, Any>("type" to "response", "success" to true, "room" to room)))
-                connections.filter { it.room == room }.forEach {
-                    it.session.send(Gson().toJson(mapOf("type" to "remote", "action" to "join")))
-                }
+                // Ask the client for credentials
+                send(Gson().toJson(mapOf("type" to "status", "data" to "auth"))) // Send response
+                var token: String // Declare token object
 
+                // Listen for incoming data
                 for (frame in incoming) {
-                    frame as?Frame.Text ?: continue
-                    val received = frame.readText()
+                    frame as? Frame.Text ?: continue // Assure type of data
+                    val received = frame.readText() // Store incoming data
+                    val parsed: Map<*, *> // Declare parsed data
 
-                    connections.filter { it.room == room }.forEach {
-                        it.session.send(received)
+                    // Try to parse data
+                    try {
+                        parsed = Gson().fromJson(received, Map::class.java)
+                    } catch (e: Exception) {
+                        println("${Date()} [Connection-$connectionId] INFO  Received bad data, ignoring")
+                        continue
+                    }
+
+                    // Process data
+                    when (parsed["type"])
+                    {
+                        "auth" -> {
+                            thisConnection = Connection(this, 0)
+                            connections += thisConnection
+                            println("${Date()} [Connection-$connectionId] INFO  User authenticated, connection accepted (${connections.size} total)")
+                        }
                     }
                 }
             } catch (e: Exception) {
-                println(e.localizedMessage)
+                e.printStackTrace()
             } finally {
-                println("User disconnected from room $room")
+                // Connection closed
+                println("${Date()} [Connection-$connectionId] INFO  Connection closed")
                 connections -= thisConnection
-                connections.filter { it.room == room }.forEach {
-                    it.session.send(Gson().toJson(mapOf("type" to "remote", "action" to "leave")))
-                }
-            }*/
+            }
         }
     }
 }
