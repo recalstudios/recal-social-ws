257fe92360ad80b2094d8b9c247bd9193be6abae
Soni <soni801_@outlook.com>
2023-12-29 16:18:50 +0100

    feat: create a basic echo websocket

--- src/websocket/websocket.gateway.ts
+++ src/websocket/websocket.gateway.ts
@@ -1,9 +1,21 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayInit, WebSocketGateway } from "@nestjs/websockets";
 
 @WebSocketGateway()
-export class WebsocketGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+export class WebsocketGateway implements OnGatewayInit
+{
+  afterInit(server: any)
+  {
+    server.on('connection', (ws: WebSocket) =>
+    {
+      // TODO: Generate a connection ID and log it
+
+      ws.onmessage = (event: MessageEvent) =>
+      {
+        let payload = event.data;
+
+        // Return data to sender
+        ws.send(`got data: ${payload}`);
+      }
+    });
   }
 }
